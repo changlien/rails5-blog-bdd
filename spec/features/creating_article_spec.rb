35ea0a5e018cbfe83ad8b58b1cb0c121315f5447
changlien <changlien918@gmail.com>
2017-05-04 21:30:17 +0000

    BDD: writing acceptance test with capybara DSL for creating new article

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -4,12 +4,25 @@ RSpec.feature "Creating Articles" do
   scenario "A user creates a new article" do 
     # scenario:
     # visit root page
+    visit "/"
+    
     # click on new article
+    click_link "New Article"
+    
     # fill in title
+    fill_in 'Title', with: 'Creating a blog'
+    
     # fill in body
+    fill_in 'Body', with: 'Lorem Ipsum'
+
     # create article
-    # expect:
+    click_button 'Create Article'
+    
+    # expections:
     # article has created
+    expect(page).to have_content('Article has been created')
+    expect(page.current_path).to eq(article_path)
+    
     # article path
   end
 end
