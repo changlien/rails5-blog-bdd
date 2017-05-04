d9859dc46686d7be591967641031d60112d4076c
changlien <changlien918@gmail.com>
2017-05-04 21:20:51 +0000

    BDD: defining the steps of test spec for creating article

---
+++ spec/features/creating_article_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.feature "Creating Articles" do
+  scenario "A user creates a new article" do 
+    # scenario:
+    # visit root page
+    # click on new article
+    # fill in title
+    # fill in body
+    # create article
+    # expect:
+    # article has created
+    # article path
+  end
+end
