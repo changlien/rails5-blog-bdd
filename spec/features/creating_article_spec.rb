51b682ea94576bb1d312aebf6183727f932b9a6f
changlien <changlien918@gmail.com>
2017-05-04 23:38:54 +0000

    add failure scenario for creating article

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -26,4 +26,29 @@ RSpec.feature "Creating Articles" do
     expect(page.current_path).to eq(articles_path)
     
   end
+  
+  scenario "A user fails to create a new article" do
+     # scenario:
+    # 1. visit root page
+    visit "/"
+    
+    # 2. click on new article
+    click_link "New Article"
+    
+    # 3. fill in title
+    fill_in 'Title', with: ''
+    
+    # 4. fill in body
+    fill_in 'Body', with: ''
+
+    # 5. create article
+    click_button 'Create Article'
+    
+    # expections:
+    # 6. article has created
+    expect(page).to have_content('Article has not been created')
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
+    
+  end
 end
